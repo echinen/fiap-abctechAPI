657c9e566999cb2f984b4a0c6c25ead0cb2dc489
Eric Chinen <7997656+echinen@users.noreply.github.com>
2023-04-30 10:57:05 -0300

    add dependency injection as contructor for version component in controller

--- src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/HealthCheckController.java
@@ -1,17 +1,22 @@
 package br.com.fiap.abctechapi.controller;
 
+import br.com.fiap.abctechapi.util.VersionComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
+    private final VersionComponent versionComponent;
+    public HealthCheckController(@Autowired VersionComponent versionComponent){
+        this.versionComponent = versionComponent;
+    }
+
     @GetMapping
     public ResponseEntity<String> status(){
         return ResponseEntity.ok("It is working (200)!!!");
@@ -19,9 +24,6 @@ public class HealthCheckController {
 
     @GetMapping("version")
     public ResponseEntity<String> version() throws IOException {
-        Properties properties = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
-        properties.load(inputStream);
-        return ResponseEntity.ok(properties.get("build_name") + " - " + properties.get("build_version"));
+        return ResponseEntity.ok(this.versionComponent.getVersion());
     }
 }
