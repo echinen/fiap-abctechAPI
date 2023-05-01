bc529b49b52bcbe85b625811ef0b5b2939787884
Eric Chinen <7997656+echinen@users.noreply.github.com>
2023-05-01 19:05:51 -0300

    add unit test for min and max flows

--- src/test/java/br/com/fiap/abctechapi/service/OrderServiceTest.java
+++ src/test/java/br/com/fiap/abctechapi/service/OrderServiceTest.java
@@ -1,18 +1,23 @@
 package br.com.fiap.abctechapi.service;
 
+import br.com.fiap.abctechapi.entity.Assistance;
 import br.com.fiap.abctechapi.entity.Order;
+import br.com.fiap.abctechapi.handler.exception.MaximunAssistRequiredException;
+import br.com.fiap.abctechapi.handler.exception.MinimunAssistRequiredException;
 import br.com.fiap.abctechapi.repository.IAssistanceRepository;
 import br.com.fiap.abctechapi.repository.IOrderRepository;
 import br.com.fiap.abctechapi.service.implementation.OrderService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class OrderServiceTest {
@@ -27,18 +32,24 @@ public class OrderServiceTest {
     public void init() {
         MockitoAnnotations.openMocks(this);
         orderService = new OrderService(assistanceRepository, orderRepository);
+        Mockito.when(assistanceRepository.findById(Mockito.any())).thenReturn(Optional.of(new Assistance(1L,"test", "description test")));
     }
 
-    // MINIMO ASSISTENCIA > 0
     @Test
     public void create_order_error_min_assist() {
         Order newOrder = new Order();
         newOrder.setOperatorId(123L);
-        Assertions.assertThrows(Exception.class, () -> orderService.saveOrder(newOrder, List.of()));
+        Assertions.assertThrows(MinimunAssistRequiredException.class, () -> orderService.saveOrder(newOrder, List.of()));
         Mockito.verify(orderRepository, Mockito.times(0)).save(newOrder);
     }
 
-    // TODO: MAXIMO ASSISTENCIA <= 15
+    @Test
+    public void create_order_error_max_assist() {
+        Order newOrder = new Order();
+        newOrder.setOperatorId(123L);
+        Assertions.assertThrows(MaximunAssistRequiredException.class, () -> orderService.saveOrder(newOrder, List.of(1L,2L,3L,4L,5L,6L,7L,8L,9L,10L,11L,12L,13L,14L,15L,16L)));
+        Mockito.verify(orderRepository, Mockito.times(0)).save(newOrder);
+    }
 
 
     // TODO: CENARIO CRIANDO ORDER
