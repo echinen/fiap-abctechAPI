df3ea08063137c8e105f8c1ee3541d8b7240a1dc
Eric Chinen <7997656+echinen@users.noreply.github.com>
2023-05-13 11:47:11 -0300

    add 1 more unit test for create order success scenario (service layer)

--- src/test/java/br/com/fiap/abctechapi/service/OrderServiceTest.java
+++ src/test/java/br/com/fiap/abctechapi/service/OrderServiceTest.java
@@ -10,7 +10,6 @@ import br.com.fiap.abctechapi.service.implementation.OrderService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +17,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
 
 @SpringBootTest
 public class OrderServiceTest {
@@ -40,7 +42,7 @@ public class OrderServiceTest {
         Order newOrder = new Order();
         newOrder.setOperatorId(123L);
         Assertions.assertThrows(MinimunAssistRequiredException.class, () -> orderService.saveOrder(newOrder, List.of()));
-        Mockito.verify(orderRepository, Mockito.times(0)).save(newOrder);
+        Mockito.verify(orderRepository, times(0)).save(newOrder);
     }
 
     @Test
@@ -48,9 +50,37 @@ public class OrderServiceTest {
         Order newOrder = new Order();
         newOrder.setOperatorId(123L);
         Assertions.assertThrows(MaximunAssistRequiredException.class, () -> orderService.saveOrder(newOrder, List.of(1L,2L,3L,4L,5L,6L,7L,8L,9L,10L,11L,12L,13L,14L,15L,16L)));
-        Mockito.verify(orderRepository, Mockito.times(0)).save(newOrder);
+        Mockito.verify(orderRepository, times(0)).save(newOrder);
     }
 
+    @Test
+    public void create_order_success() throws Exception {
+        Order newOrder = new Order();
 
-    // TODO: CENARIO CRIANDO ORDER
+        // Create mock data
+        newOrder.setOperatorId(1234L);
+
+        List<Long> assists = List.of(1L, 2L, 3L);
+
+        Assistance assistance1 = new Assistance();
+        assistance1.setId(1L);
+
+        Assistance assistance2 = new Assistance();
+        assistance2.setId(2L);
+
+        Assistance assistance3 = new Assistance();
+        assistance3.setId(3L);
+
+        // Mock the behavior of assistanceRepository.findById
+        Mockito.when(assistanceRepository.findById(1L)).thenReturn(Optional.of(assistance1));
+        Mockito.when(assistanceRepository.findById(2L)).thenReturn(Optional.of(assistance2));
+        Mockito.when(assistanceRepository.findById(3L)).thenReturn(Optional.of(assistance3));
+
+        // Call the method
+        orderService.saveOrder(newOrder, assists);
+
+        // Verify the behavior
+        Mockito.verify(assistanceRepository, times(3)).findById(anyLong());
+        Mockito.verify(orderRepository, times(1)).save(newOrder);
+    }
 }
