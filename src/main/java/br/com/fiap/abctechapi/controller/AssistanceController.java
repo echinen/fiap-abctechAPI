ab3067510d2d61ab89da4ae40e629b879ba35435
Zanderlan Neto <zanderlan.n@gmail.com>
2023-05-22 21:28:22 -0300

    fix cors

--- src/main/java/br/com/fiap/abctechapi/controller/AssistanceController.java
+++ src/main/java/br/com/fiap/abctechapi/controller/AssistanceController.java
@@ -7,11 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("/assists")
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS})
 public class AssistanceController {
     private final AssistanceApp assistanceApp;
 
